3551c3ba64d8c9c00e5ef22c14ef827124820c7f
saisuma-nsk1 <56062165+saisuma-nsk1@users.noreply.github.com>
2019-11-18 10:44:34 +0530

    master

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ require('http')
   .createServer((req, res) => {
     console.log(`incoming url: ${req.url} and incoming method: ${req.method}`)
     res.writeHeader(200,{'Content-Type': 'text/html'})
-    res.write('<p>Buenos dias Montevideo</p>')
-    res.write('<p>This is Saturday. Bla blah blah</p>')
-    res.end('<h1>Hello World from amazing Node University course AWS Intermediate</h1>')
+    res.write('<p>AWS Codepipeline</p>')
+    res.write('<p>This is Nodejs application</p>')
+    res.end('<h1>Hello Zenqore</h1>')
   })
   .listen(port, (error)=>{
     console.log(`server is running on ${port}`)
